65e778257f5f61b0ae39fac4f31f715c03b9a64f
nada <nada@alphamesh.xyz>
2024-12-24 10:39:58 +0800

    feat: flake env

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        # Select the rust toolchain you want
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust toolchain
+            rustToolchain
+
+            # WASM tools
+            wasm-pack
+
+            # Common build tools
+            pkg-config
+            cmake
+            
+            # Development tools
+            rust-analyzer
+            clippy
+            rustfmt
+
+            git
+          ];
+
+          # Set environment variables
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
