eb5acae6039ae1f35d52a85d49bda85040904475
nada <nada@alphamesh.xyz>
2024-12-25 11:04:22 +0800

    fix: comments

--- src/lib.rs
+++ src/lib.rs
@@ -360,139 +360,139 @@ fn parse_instruction<'a>(
 //     Ok(Some(row))
 // }
 
-// fn parse_raydium_amm_instruction<'a>(
-//     instruction: &IndexedInstruction,
-//     context: &TransactionContext,
-//     tables: &'a mut Tables,
-//     slot: u64,
-//     transaction_index: u32,
-// ) -> Result<Option<&'a mut Row>, Error> {
-//     let row = match raydium_amm_substream::parse_instruction(&instruction.instruction, context).map_err(|x| anyhow!(x))? {
-//         Some(raydium_amm_event::Event::Swap(swap)) => {
-//             tables.create_row("raydium_amm_swap_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("amm", &swap.amm)
-//                 .set("user", &swap.user)
-//                 .set("amount_in", swap.amount_in)
-//                 .set("amount_out", swap.amount_out)
-//                 .set("mint_in", &swap.mint_in)
-//                 .set("mint_out", &swap.mint_out)
-//                 .set("direction", &swap.direction)
-//                 .set("pool_pc_amount", swap.pool_pc_amount.unwrap_or(0))
-//                 .set("pool_coin_amount", swap.pool_coin_amount.unwrap_or(0))
-//                 .set("user_pre_balance_in", swap.user_pre_balance_in.unwrap_or(0))
-//                 .set("user_pre_balance_out", swap.user_pre_balance_out.unwrap_or(0))
-//         }
-//         Some(raydium_amm_event::Event::Initialize(initialize)) => {
-//             tables.create_row("raydium_amm_initialize_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("amm", &initialize.amm)
-//                 .set("user", &initialize.user)
-//                 .set("pc_init_amount", initialize.pc_init_amount)
-//                 .set("coin_init_amount", initialize.coin_init_amount)
-//                 .set("lp_init_amount", initialize.lp_init_amount)
-//                 .set("pc_mint", &initialize.pc_mint)
-//                 .set("coin_mint", &initialize.coin_mint)
-//                 .set("lp_mint", &initialize.lp_mint)
-//                 .set("user_pc_pre_balance", initialize.user_pc_pre_balance.unwrap_or(0))
-//                 .set("user_coin_pre_balance", initialize.user_coin_pre_balance.unwrap_or(0))
-//         },
-//         Some(raydium_amm_event::Event::Deposit(deposit)) => {
-//             tables.create_row("raydium_amm_deposit_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("amm", &deposit.amm)
-//                 .set("user", &deposit.user)
-//                 .set("pc_amount", deposit.pc_amount)
-//                 .set("coin_amount", deposit.coin_amount)
-//                 .set("pool_pc_amount", deposit.pool_pc_amount.unwrap_or(0))
-//                 .set("pool_coin_amount", deposit.pool_coin_amount.unwrap_or(0))
-//                 .set("lp_amount", deposit.lp_amount)
-//                 .set("pc_mint", &deposit.pc_mint)
-//                 .set("coin_mint", &deposit.coin_mint)
-//                 .set("lp_mint", &deposit.lp_mint)
-//                 .set("user_pc_pre_balance", deposit.user_pc_pre_balance.unwrap_or(0))
-//                 .set("user_coin_pre_balance", deposit.user_coin_pre_balance.unwrap_or(0))
-//         },
-//         Some(raydium_amm_event::Event::Withdraw(withdraw)) => {
-//             tables.create_row("raydium_amm_withdraw_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("amm", &withdraw.amm)
-//                 .set("user", &withdraw.user)
-//                 .set("pc_amount", withdraw.pc_amount)
-//                 .set("coin_amount", withdraw.coin_amount)
-//                 .set("pool_pc_amount", withdraw.pool_pc_amount.unwrap_or(0))
-//                 .set("pool_coin_amount", withdraw.pool_coin_amount.unwrap_or(0))
-//                 .set("lp_amount", withdraw.lp_amount)
-//                 .set("pc_mint", &withdraw.pc_mint)
-//                 .set("coin_mint", &withdraw.coin_mint)
-//                 .set("lp_mint", &withdraw.lp_mint)
-//                 .set("user_pc_pre_balance", withdraw.user_pc_pre_balance.unwrap_or(0))
-//                 .set("user_coin_pre_balance", withdraw.user_coin_pre_balance.unwrap_or(0))
-//         },
-//         Some(raydium_amm_event::Event::WithdrawPnl(withdraw_pnl)) => {
-//             tables.create_row("raydium_amm_withdraw_pnl_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("amm", withdraw_pnl.amm)
-//                 .set("user", withdraw_pnl.user)
-//                 .set("pc_amount", withdraw_pnl.pc_amount.unwrap_or(0))
-//                 .set("coin_amount", withdraw_pnl.coin_amount.unwrap_or(0))
-//                 .set("pc_mint", withdraw_pnl.pc_mint.unwrap_or("".to_string()))
-//                 .set("coin_mint", withdraw_pnl.coin_mint.unwrap_or("".to_string()))
-//         }
-//         _ => return Ok(None),
-//     };
-//     Ok(Some(row))
-// }
+fn parse_raydium_amm_instruction<'a>(
+    instruction: &IndexedInstruction,
+    context: &TransactionContext,
+    tables: &'a mut Tables,
+    slot: u64,
+    transaction_index: u32,
+) -> Result<Option<&'a mut Row>, Error> {
+    let row = match raydium_amm_substream::parse_instruction(&instruction.instruction, context).map_err(|x| anyhow!(x))? {
+        Some(raydium_amm_event::Event::Swap(swap)) => {
+            tables.create_row("raydium_amm_swap_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("amm", &swap.amm)
+                .set("user", &swap.user)
+                .set("amount_in", swap.amount_in)
+                .set("amount_out", swap.amount_out)
+                .set("mint_in", &swap.mint_in)
+                .set("mint_out", &swap.mint_out)
+                .set("direction", &swap.direction)
+                .set("pool_pc_amount", swap.pool_pc_amount.unwrap_or(0))
+                .set("pool_coin_amount", swap.pool_coin_amount.unwrap_or(0))
+                .set("user_pre_balance_in", swap.user_pre_balance_in.unwrap_or(0))
+                .set("user_pre_balance_out", swap.user_pre_balance_out.unwrap_or(0))
+        }
+        Some(raydium_amm_event::Event::Initialize(initialize)) => {
+            tables.create_row("raydium_amm_initialize_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("amm", &initialize.amm)
+                .set("user", &initialize.user)
+                .set("pc_init_amount", initialize.pc_init_amount)
+                .set("coin_init_amount", initialize.coin_init_amount)
+                .set("lp_init_amount", initialize.lp_init_amount)
+                .set("pc_mint", &initialize.pc_mint)
+                .set("coin_mint", &initialize.coin_mint)
+                .set("lp_mint", &initialize.lp_mint)
+                .set("user_pc_pre_balance", initialize.user_pc_pre_balance.unwrap_or(0))
+                .set("user_coin_pre_balance", initialize.user_coin_pre_balance.unwrap_or(0))
+        },
+        Some(raydium_amm_event::Event::Deposit(deposit)) => {
+            tables.create_row("raydium_amm_deposit_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("amm", &deposit.amm)
+                .set("user", &deposit.user)
+                .set("pc_amount", deposit.pc_amount)
+                .set("coin_amount", deposit.coin_amount)
+                .set("pool_pc_amount", deposit.pool_pc_amount.unwrap_or(0))
+                .set("pool_coin_amount", deposit.pool_coin_amount.unwrap_or(0))
+                .set("lp_amount", deposit.lp_amount)
+                .set("pc_mint", &deposit.pc_mint)
+                .set("coin_mint", &deposit.coin_mint)
+                .set("lp_mint", &deposit.lp_mint)
+                .set("user_pc_pre_balance", deposit.user_pc_pre_balance.unwrap_or(0))
+                .set("user_coin_pre_balance", deposit.user_coin_pre_balance.unwrap_or(0))
+        },
+        Some(raydium_amm_event::Event::Withdraw(withdraw)) => {
+            tables.create_row("raydium_amm_withdraw_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("amm", &withdraw.amm)
+                .set("user", &withdraw.user)
+                .set("pc_amount", withdraw.pc_amount)
+                .set("coin_amount", withdraw.coin_amount)
+                .set("pool_pc_amount", withdraw.pool_pc_amount.unwrap_or(0))
+                .set("pool_coin_amount", withdraw.pool_coin_amount.unwrap_or(0))
+                .set("lp_amount", withdraw.lp_amount)
+                .set("pc_mint", &withdraw.pc_mint)
+                .set("coin_mint", &withdraw.coin_mint)
+                .set("lp_mint", &withdraw.lp_mint)
+                .set("user_pc_pre_balance", withdraw.user_pc_pre_balance.unwrap_or(0))
+                .set("user_coin_pre_balance", withdraw.user_coin_pre_balance.unwrap_or(0))
+        },
+        Some(raydium_amm_event::Event::WithdrawPnl(withdraw_pnl)) => {
+            tables.create_row("raydium_amm_withdraw_pnl_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("amm", withdraw_pnl.amm)
+                .set("user", withdraw_pnl.user)
+                .set("pc_amount", withdraw_pnl.pc_amount.unwrap_or(0))
+                .set("coin_amount", withdraw_pnl.coin_amount.unwrap_or(0))
+                .set("pc_mint", withdraw_pnl.pc_mint.unwrap_or("".to_string()))
+                .set("coin_mint", withdraw_pnl.coin_mint.unwrap_or("".to_string()))
+        }
+        _ => return Ok(None),
+    };
+    Ok(Some(row))
+}
 
-// fn parse_pumpfun_instruction<'a>(
-//     instruction: &IndexedInstruction,
-//     context: &TransactionContext,
-//     tables: &'a mut Tables,
-//     slot: u64,
-//     transaction_index: u32,
-// ) -> Result<Option<&'a mut Row>, Error> {
-//     let row = match pumpfun_substream::parse_instruction(&instruction.instruction, context)? {
-//         Some(pumpfun_event::Event::Create(create)) => {
-//             tables.create_row("pumpfun_create_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("user", create.user)
-//                 .set("name", create.name)
-//                 .set("symbol", create.symbol)
-//                 .set("uri", create.uri)
-//                 .set("mint", create.mint)
-//                 .set("bonding_curve", create.bonding_curve)
-//                 .set("associated_bonding_curve", create.associated_bonding_curve)
-//                 .set("metadata", create.metadata)
-//         },
-//         Some(pumpfun_event::Event::Initialize(initialize)) => {
-//             tables.create_row("pumpfun_initialize_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("user", initialize.user)
-//         },
-//         Some(pumpfun_event::Event::SetParams(set_params)) => {
-//             tables.create_row("pumpfun_set_params_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("user", set_params.user)
-//                 .set("fee_recipient", set_params.fee_recipient)
-//                 .set("initial_virtual_token_reserves", set_params.initial_virtual_token_reserves)
-//                 .set("initial_virtual_sol_reserves", set_params.initial_virtual_sol_reserves)
-//                 .set("initial_real_token_reserves", set_params.initial_real_token_reserves)
-//                 .set("token_total_supply", set_params.token_total_supply)
-//                 .set("fee_basis_points", set_params.fee_basis_points)
-//         },
-//         Some(pumpfun_event::Event::Swap(swap)) => {
-//             tables.create_row("pumpfun_swap_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("user", swap.user)
-//                 .set("mint", swap.mint)
-//                 .set("bonding_curve", swap.bonding_curve)
-//                 .set("token_amount", swap.token_amount)
-//                 .set("direction", swap.direction)
-//                 .set("sol_amount", swap.sol_amount.unwrap_or(0))
-//                 .set("virtual_sol_reserves", swap.virtual_sol_reserves.unwrap_or(0))
-//                 .set("virtual_token_reserves", swap.virtual_token_reserves.unwrap_or(0))
-//                 .set("real_sol_reserves", swap.real_sol_reserves.unwrap_or(0))
-//                 .set("real_token_reserves", swap.real_token_reserves.unwrap_or(0))
-//         },
-//         Some(pumpfun_event::Event::Withdraw(withdraw)) => {
-//             tables.create_row("pumpfun_withdraw_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
-//                 .set("mint", withdraw.mint)
-//         },
-//         None => return Ok(None)
-//     };
-//     Ok(Some(row))
-// }
+fn parse_pumpfun_instruction<'a>(
+    instruction: &IndexedInstruction,
+    context: &TransactionContext,
+    tables: &'a mut Tables,
+    slot: u64,
+    transaction_index: u32,
+) -> Result<Option<&'a mut Row>, Error> {
+    let row = match pumpfun_substream::parse_instruction(&instruction.instruction, context)? {
+        Some(pumpfun_event::Event::Create(create)) => {
+            tables.create_row("pumpfun_create_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("user", create.user)
+                .set("name", create.name)
+                .set("symbol", create.symbol)
+                .set("uri", create.uri)
+                .set("mint", create.mint)
+                .set("bonding_curve", create.bonding_curve)
+                .set("associated_bonding_curve", create.associated_bonding_curve)
+                .set("metadata", create.metadata)
+        },
+        Some(pumpfun_event::Event::Initialize(initialize)) => {
+            tables.create_row("pumpfun_initialize_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("user", initialize.user)
+        },
+        Some(pumpfun_event::Event::SetParams(set_params)) => {
+            tables.create_row("pumpfun_set_params_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("user", set_params.user)
+                .set("fee_recipient", set_params.fee_recipient)
+                .set("initial_virtual_token_reserves", set_params.initial_virtual_token_reserves)
+                .set("initial_virtual_sol_reserves", set_params.initial_virtual_sol_reserves)
+                .set("initial_real_token_reserves", set_params.initial_real_token_reserves)
+                .set("token_total_supply", set_params.token_total_supply)
+                .set("fee_basis_points", set_params.fee_basis_points)
+        },
+        Some(pumpfun_event::Event::Swap(swap)) => {
+            tables.create_row("pumpfun_swap_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("user", swap.user)
+                .set("mint", swap.mint)
+                .set("bonding_curve", swap.bonding_curve)
+                .set("token_amount", swap.token_amount)
+                .set("direction", swap.direction)
+                .set("sol_amount", swap.sol_amount.unwrap_or(0))
+                .set("virtual_sol_reserves", swap.virtual_sol_reserves.unwrap_or(0))
+                .set("virtual_token_reserves", swap.virtual_token_reserves.unwrap_or(0))
+                .set("real_sol_reserves", swap.real_sol_reserves.unwrap_or(0))
+                .set("real_token_reserves", swap.real_token_reserves.unwrap_or(0))
+        },
+        Some(pumpfun_event::Event::Withdraw(withdraw)) => {
+            tables.create_row("pumpfun_withdraw_events", [("slot", slot.to_string()), ("transaction_index", transaction_index.to_string()), ("instruction_index", instruction.index.to_string())])
+                .set("mint", withdraw.mint)
+        },
+        None => return Ok(None)
+    };
+    Ok(Some(row))
+}
 
 // fn parse_mpl_token_metadata_instruction<'a>(
 //     instruction: &IndexedInstruction,
